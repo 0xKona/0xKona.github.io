9143160297140f70d382165fb6c36703eff359ed
Kona <konarobinson@outlook.com>
2023-11-29 18:22:11 +0000

    styled components / typescript styled components added

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
+import Header from "./components/header";
 
 const App = () => {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <h1>Running</h1>
-            <p>{`Count: ${count}.`}</p>
-            <button onClick={() => setCount(count+1)}>click to increase count</button>
+            <Header />
         </>
     )
 }
